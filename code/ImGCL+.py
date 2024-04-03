1e11402889d1e8a11d3707c3dc7bad63523ac819
wewixn <2368338448@qq.com>
2024-04-03 22:58:58 +0800

    fix cluster && add smote

--- code/ImGCL+.py
+++ code/ImGCL+.py
@@ -2,20 +2,38 @@ import torch
 import os.path as osp
 import GCL.losses as L
 import GCL.augmentors as A
+import torch.nn.functional as F
 import torch_geometric.transforms as T
-from torch_geometric.utils import add_self_loops, degree, subgraph, to_dense_adj
-
-from imblearn.under_sampling import RandomUnderSampler, TomekLinks
-import numpy as np
 from sklearn.cluster import KMeans
+from torch_geometric.utils import add_self_loops, degree, subgraph, to_dense_adj, dense_to_sparse
+
+import random
+from copy import deepcopy
+from scipy.spatial.distance import pdist, squareform
+from imblearn.under_sampling import TomekLinks, NeighbourhoodCleaningRule, NearMiss
+import numpy as np
 from tqdm import tqdm
 from torch.optim import Adam
 from GCL.eval import get_split, LREvaluator
-from Contrast import WithinEmbedContrast
-# from GCL.models.contrast_model import WithinEmbedContrast
-from torch_geometric.nn import GCNConv
-from torch_geometric.datasets import DBLP, Amazon, WikiCS
+from GCL.models.contrast_model import WithinEmbedContrast
+from torch_geometric.nn import GCNConv, GATConv
+from torch_geometric.datasets import Amazon, WikiCS, Planetoid
 from pl_bolts.optimizers import LinearWarmupCosineAnnealingLR
+
+
+class GAT(torch.nn.Module):
+    def __init__(self, num_features, num_hidden):
+        super(GAT, self).__init__()
+        self.conv1 = GATConv(num_features, num_hidden, heads=8, concat=True)
+        self.conv2 = GATConv(num_hidden * 8, num_hidden, heads=1, concat=False)
+
+    def forward(self, x, edge_index, edge_weight=None):
+        z = F.dropout(x, p=0.6, training=self.training)
+        z = self.conv1(z, edge_index, edge_weight)
+        z = F.elu(z)
+        z = F.dropout(z, p=0.6, training=self.training)
+        z = self.conv2(z, edge_index, edge_weight)
+        return z
 
 
 class GConv(torch.nn.Module):
@@ -69,7 +87,7 @@ def pbs_sample(data, pseudo_labels, a, alpha=0.85, pr=0.1, l=0.1):
     row, col = edge_self_loop
     deg = degree(col, data.num_nodes, dtype=data.x.dtype)
     deg_inv = deg.pow(-1)
-    adj_matrix = to_dense_adj(edge_self_loop, edge_attr=data.edge_attr)
+    adj_matrix = to_dense_adj(edge_self_loop, edge_attr=data.edge_attr)[0]
     node_centrality = alpha * adj_matrix @ deg_inv + torch.ones(data.num_nodes).to(device)
     nc_max = torch.max(node_centrality)
     nc_min = torch.min(node_centrality)
@@ -83,10 +101,11 @@ def pbs_sample(data, pseudo_labels, a, alpha=0.85, pr=0.1, l=0.1):
 
 
 def sim_sample(data, pseudo_labels, sampler=None, encoder_model=None):
+    device = data.x.device
     data_clone = data.clone()
 
     if encoder_model is not None:
-        data4sample, _, _ = encoder_model(data_clone.x, data_clone.edge_index, data_clone.edge_attr)
+        data4sample = encoder_model(data_clone.x, data_clone.edge_index, data_clone.edge_attr)
         data4sample = data4sample.detach().cpu().numpy()
     else:
         data4sample = data_clone.x.detach().cpu().numpy()
@@ -95,26 +114,144 @@ def sim_sample(data, pseudo_labels, sampler=None, encoder_model=None):
         sampler = TomekLinks()
 
     _, pseudo_labels = sampler.fit_resample(data4sample, pseudo_labels.cpu().numpy())
-    pseudo_labels = torch.from_numpy(pseudo_labels).to(device=data.x.device)
-    sample_mask = sampler.sample_indices_
+    pseudo_labels = torch.from_numpy(pseudo_labels).to(device=device)
+    sample_mask = torch.from_numpy(sampler.sample_indices_).to(device=device)
 
-    sample_mask = torch.from_numpy(sample_mask).to(device=data.x.device)
-    data.x = data_clone.x[sample_mask]
-    data.edge_index = subgraph(sample_mask, data_clone.edge_index, num_nodes=data_clone.x.size(0))[0]
-    data.edge_index = remap_edge_index(data.edge_index, sample_mask, data_clone.x.size(0))
+    data_clone.x = data.x[sample_mask]
+    data_clone.edge_index = subgraph(sample_mask, data.edge_index, num_nodes=data.num_nodes)[0]
+    data_clone.edge_index = remap_edge_index(data_clone.edge_index, sample_mask, data.num_nodes)
 
-    return data, pseudo_labels
+    return data_clone, pseudo_labels
 
 
-def cluster(encoder_model, data, num_clusters=100):
-    z, _, _ = encoder_model(data.x, data.edge_index, data.edge_attr)
+def src_smote(adj, features, labels, portion=1.0, im_class_num=3):
+    cluster_counts = torch.bincount(labels)
+    avg_number = int(labels.size(0) / cluster_counts.size(0))
+    minority_clusters = cluster_counts[cluster_counts < avg_number].size(0)
+    im_class_num = min(im_class_num, minority_clusters)
+
+    sample_idx = torch.argsort(cluster_counts)[:im_class_num]
+    adj_back = adj.to_dense()
+    chosen = None
+    new_features = None
+
+    idx_train = torch.arange(labels.shape[0]).to(device=adj.device)
+
+    for i in range(im_class_num):
+        new_chosen = idx_train[(labels == (sample_idx[i]))[idx_train]]
+        if portion == 0:  # refers to even distribution
+            c_portion = int(avg_number / new_chosen.shape[0])
+
+            portion_rest = (avg_number / new_chosen.shape[0]) - c_portion
+
+        else:
+            c_portion = int(portion)
+            portion_rest = portion - c_portion
+
+        for j in range(c_portion):
+            num = int(new_chosen.shape[0])
+            new_chosen = new_chosen[:num]
+
+            chosen_embed = features[new_chosen, :]
+            distance = squareform(pdist(chosen_embed.cpu().detach()))
+            np.fill_diagonal(distance, distance.max() + 100)
+
+            idx_neighbor = distance.argmin(axis=-1)
+
+            interp_place = random.random()
+            embed = chosen_embed + (chosen_embed[idx_neighbor, :] - chosen_embed) * interp_place
+
+            if chosen is None:
+                chosen = new_chosen
+                new_features = embed
+            else:
+                chosen = torch.cat((chosen, new_chosen), 0)
+                new_features = torch.cat((new_features, embed), 0)
+
+        num = int(new_chosen.shape[0] * portion_rest)
+        new_chosen = new_chosen[:num]
+        if num == 0:
+            continue
+
+        chosen_embed = features[new_chosen, :]
+        distance = squareform(pdist(chosen_embed.cpu().detach()))
+        np.fill_diagonal(distance, distance.max() + 100)
+
+        idx_neighbor = distance.argmin(axis=-1)
+
+        interp_place = random.random()
+        embed = chosen_embed + (chosen_embed[idx_neighbor, :] - chosen_embed) * interp_place
+
+        if chosen is None:
+            chosen = new_chosen
+            new_features = embed
+        else:
+            chosen = torch.cat((chosen, new_chosen), 0)
+            new_features = torch.cat((new_features, embed), 0)
+
+    add_num = chosen.shape[0]
+    new_adj = adj_back.new(torch.Size((adj_back.shape[0] + add_num, adj_back.shape[0] + add_num)))
+    new_adj[:adj_back.shape[0], :adj_back.shape[0]] = adj_back[:, :]
+    new_adj[adj_back.shape[0]:, :adj_back.shape[0]] = adj_back[chosen, :]
+    new_adj[:adj_back.shape[0], adj_back.shape[0]:] = adj_back[:, chosen]
+    new_adj[adj_back.shape[0]:, adj_back.shape[0]:] = adj_back[chosen, :][:, chosen]
+
+    features_append = deepcopy(new_features)
+    labels_append = deepcopy(labels[chosen])
+
+    features = torch.cat((features, features_append), 0)
+    labels = torch.cat((labels, labels_append), 0)
+    adj = new_adj
+
+    return adj, features, labels
+
+
+def over_sample(data, pseudo_labels, portion=0.0):
+    device = data.x.device
+    data_clone = data.clone()
+
+    # align dims
+    adj_part = to_dense_adj(data.edge_index)[0]
+    adj = torch.zeros((data_clone.num_nodes, data_clone.num_nodes)).to(device=device)
+    adj[:adj_part.size(0), :adj_part.size(1)] = adj_part
+    adj, features, labels = src_smote(adj, data_clone.x, pseudo_labels, portion=portion)
+    data_clone.x = features
+    data_clone.edge_index = dense_to_sparse(adj)[0]
+
+    return data_clone, pseudo_labels
+
+
+def cluster(encoder_model, data, num_clusters=10):
+    device = data.x.device
+    z = encoder_model(data.x, data.edge_index, data.edge_attr)
 
     data_array = z.cpu().detach().numpy()
 
     kmeans = KMeans(n_clusters=num_clusters, random_state=0)
     cluster_labels = kmeans.fit_predict(data_array)
-    cluster_labels = torch.from_numpy(cluster_labels)
 
+    # process small clusters
+    min_cluster_size = min(30, data.x.size(0) / 3 / num_clusters)
+    unique_labels, counts = np.unique(cluster_labels, return_counts=True)
+    small_clusters = unique_labels[counts < min_cluster_size].tolist()
+    centers = kmeans.cluster_centers_
+    while small_clusters:
+        small_cluster = small_clusters[0]
+        distances = np.linalg.norm(centers - centers[small_cluster], axis=1)
+        distances[small_cluster] = np.inf
+        nearest_cluster = np.argmin(distances)
+        centers[nearest_cluster] = ((counts[nearest_cluster] * centers[nearest_cluster] +
+                                     counts[small_cluster] * centers[small_cluster]) /
+                                    (counts[nearest_cluster] + counts[small_cluster]))
+        counts[nearest_cluster] += counts[small_cluster]
+        centers = np.delete(centers, small_cluster, axis=0)
+        counts = np.delete(counts, small_cluster)
+        cluster_labels[cluster_labels == small_cluster] = nearest_cluster
+        cluster_labels[cluster_labels > small_cluster] -= 1
+        unique_labels = np.unique(cluster_labels)
+        small_clusters = unique_labels[counts < min_cluster_size].tolist()
+
+    cluster_labels = torch.from_numpy(cluster_labels).to(device)
     return cluster_labels
 
 
@@ -130,12 +267,6 @@ def edge_perturbation(edge_index, mask, disconnect_prob=0.05):
     unchanged_edges = edge_index[:, ~mask]
     edge_index = np.concatenate([unchanged_edges, disconnected_edges], axis=1)
     return edge_index
-
-
-def pos_perturbation(data, ):
-    data = T.RandomRotate(degrees=10, axis=0)(data)
-    data = T.RandomTranslate(0.1)(data)
-    return data
 
 
 def rand_noise(data, labels):
@@ -156,36 +287,14 @@ def rand_noise(data, labels):
     data.edge_index = edge_perturbation(data.edge_index, mask, disconnect_prob=0.01)
     data.edge_index = torch.from_numpy(data.edge_index).to(device=data.x.device)
 
-    # add noise to position
-    # data = pos_perturbation(data, )
-
     return data
 
 
-def generate_sampling_strategy(labels):
-    labels = labels.cpu().numpy()
-    class_counts = np.bincount(labels)
-    class_threshold = len(labels) / len(class_counts)
-    max_minority_count = min(class_counts)
-    for class_count in enumerate(class_counts):
-        if class_count < class_threshold:
-            max_minority_count = max(max_minority_count, class_count)
-    sampling_strategy = {}
-
-    for class_label, class_count in enumerate(class_counts):
-        if class_count < class_threshold:
-            sampling_strategy[class_label] = class_count
-        else:
-            sampling_strategy[class_label] = max(int(0.5 * class_count), max_minority_count)
-
-    return sampling_strategy
-
-
-def train(encoder_model, contrast_model, data, optimizer, pseudo_labels=None):
+def train(encoder_model, contrast_model, data, optimizer):
     encoder_model.train()
     optimizer.zero_grad()
     _, z1, z2 = encoder_model(data.x, data.edge_index, data.edge_attr)
-    loss = contrast_model(z1, z2, pseudo_labels)
+    loss = contrast_model(z1, z2)
     loss.backward()
     optimizer.step()
     return loss.item()
@@ -201,15 +310,15 @@ def test(encoder_model, data):
 
 def main():
     device = torch.device('cuda')
-    path = osp.join(osp.expanduser('~'), 'datasets', 'Amazon')
-    dataset = Amazon(path, name='Computers', transform=T.NormalizeFeatures())
+    path = osp.join(osp.expanduser('.'), 'datasets', 'Amazon')
+    dataset = Amazon(path, name='computers', transform=T.NormalizeFeatures())
     data = dataset[0].to(device)
-    data_train = data.clone()
 
     aug1 = A.Compose([A.EdgeRemoving(pe=0.5), A.FeatureMasking(pf=0.1)])
     aug2 = A.Compose([A.EdgeRemoving(pe=0.5), A.FeatureMasking(pf=0.1)])
 
     gconv = GConv(input_dim=dataset.num_features, hidden_dim=256).to(device)
+    gat = GAT(num_features=dataset.num_features, num_hidden=64).to(device)
     encoder_model = Encoder(encoder=gconv, augmentor=(aug1, aug2)).to(device)
     contrast_model = WithinEmbedContrast(loss=L.BarlowTwins()).to(device)
 
@@ -219,30 +328,35 @@ def main():
         warmup_epochs=400,
         max_epochs=4000)
 
-    B = 100
-    num_clusters = min(int(data.x.size(0) / 100), 100)
-    pseudo_labels = cluster(encoder_model, data, num_clusters=num_clusters).to(device)
+    B = 200
+    data_train = data.clone()
     with tqdm(total=4000, desc='(T)') as pbar:
         for epoch in range(1, 4001):
             loss = train(encoder_model, contrast_model, data_train, optimizer)
+            optimizer.zero_grad()
             if epoch % B == 0:
                 # num_clusters = min(int(data.x.size(0) / 100), 100)
-                # pseudo_labels = cluster(encoder_model, data, num_clusters=num_clusters).to(device)
+                # pseudo_labels = cluster(encoder_model.encoder, data, num_clusters=num_clusters).to(device)
                 # sample_mask = pbs_sample(data, pseudo_labels, 1-epoch/40)
                 # data_train.x = data.x[sample_mask]
                 # data_train.edge_index = subgraph(sample_mask, data.edge_index, num_nodes=data.x.size(0))[0]
                 # data_train.edge_index = remap_edge_index(data_train.edge_index, sample_mask, data.x.size(0))
                 # pseudo_labels = pseudo_labels[sample_mask]
 
-                prev_data_count = data_train.x.size(0)
-                undersampler = TomekLinks()
-                data_train, pseudo_labels = sim_sample(data_train, pseudo_labels, undersampler, encoder_model)
-
-                # data_train = rand_noise(data_train, pseudo_labels)
-
-                if data_train.x.size(0) >= 0.99 * prev_data_count:
+                if data_train.x.size(0) <= 0.2 * data.x.size(0):
                     data_train = data.clone()
-                    pseudo_labels = cluster(encoder_model, data, num_clusters=num_clusters).to(device)
+
+                num_clusters = 100
+                pseudo_labels = cluster(encoder_model.encoder, data_train, num_clusters=num_clusters)
+                if torch.unique(pseudo_labels).size(0) == 1:
+                    data_train = data.clone()
+                    pseudo_labels = cluster(encoder_model.encoder, data_train, num_clusters=num_clusters)
+                undersampler = NearMiss(version=3, sampling_strategy='majority', n_neighbors_ver3=20)
+                data_train, pseudo_labels = sim_sample(data_train, pseudo_labels, undersampler, encoder_model.encoder)
+                data_train, pseudo_labels = over_sample(data_train, pseudo_labels, portion=0.5)
+
+                save_path = f'data/saved_data_epoch_{epoch}.pt'
+                torch.save(data_train, save_path)
 
             scheduler.step()
             pbar.set_postfix({'loss': loss})
@@ -255,7 +369,8 @@ def main():
 
 if __name__ == '__main__':
     res = []
-    for i in range(4):
+    for i in range(5):
         res.append(main())
 
-    print(res)
+    for i in range(5):
+        print(f'F1Mi={res[i]["micro_f1"]:.4f}, F1Ma={res[i]["macro_f1"]:.4f}')
